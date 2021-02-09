3637b8631532881b42c3da016e51da20f9841758
Puneet Shrivas <puneetshrivas32@gmail.com>
2021-02-09 14:21:17 +0530

    Rearranged folders

--- Article_Pipelines/Article_BlogIP.py
+++ Article_Pipelines/Article_BlogIP.py
@@ -9,18 +9,33 @@ from selenium.webdriver.support.expected_conditions import presence_of_all_eleme
 PATH = "C:\\Users\\punee\\Downloads\\chromedriver_win32\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://blog.ipleaders.in/blog/")
-WebDriverWait(driver,30).until(presence_of_all_elements_located((By.CLASS_NAME,"popautoscroll")))
-close_overlay = driver.find_elements_by_xpath("//*[@id='shwPopsCls']")
-close_overlay[0].click()
-WebDriverWait(driver,10).until(presence_of_all_elements_located((By.ID,"td_block_inner")))
-blog_id = driver.find_elements_by_class_name("td_block_inner")
-blog_rows = blog_id.find_elements(By.CLASS_NAME,"td-block-row")
-for blog_row in blog_rows:
-    blocks = blog_row.find_elements(By.CLASS_NAME,"td-block-span6")
-    for block in blocks:
-        a_tags = block.find_elements(By.TAG_NAME,"a")
-        for a_tag in a_tags:
-            print(a_tag.get_attribute('title'))
-            print(a_tag.get_attribute('href'))
+# WebDriverWait(driver,120).until(EC.visibility_of_element_located((By.CLASS_NAME,"popautoscroll")))
+# close_overlay = driver.find_element_by_xpath("//*[@id='shwPopsCls']")
+# close_overlay.click()
+# WebDriverWait(driver,10).until(presence_of_all_elements_located((By.ID,"td_block_inner")))
+# while(true):
+last_count = 0
+while(1):
+    count = 0
+    blog_id = driver.find_element_by_class_name("td_block_inner")
+    blog_rows = blog_id.find_elements(By.CLASS_NAME,"td-block-row")
+    for blog_row in blog_rows:
+        count = count + 1
+        if(count<last_count): continue
+        last_count = count
+        print("count : "+ str(count))
+        print("lastcount : "+ str(last_count))
+        blocks = blog_row.find_elements(By.CLASS_NAME,"td-block-span6")
+        for block in blocks:
+            a_tag = block.find_element(By.TAG_NAME,"a")
+            # print(a_tag.get_attribute('title'))
+            # print(a_tag.get_attribute('href'))
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+def get_article(url):
+    script = "window.open('{0}', 'new_window')".format(url)
+    driver.execute_script(script)
+    driver.switch_to_window(driver.window_handles[-1])
+    print(driver.title)
 
 # driver.quit() #to close browser window
