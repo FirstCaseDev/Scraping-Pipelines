92fc91ef1efa9ed4a6e4ca70c2664553a6ee94c2
Pushpak Ladhe <ladhepushpak@gmail.com>
2021-02-14 16:47:10 +0530

    update (Calendar)
    Calendar Function updated

--- Court_Data_Pipelines/Delhi_High_Court.py
+++ Court_Data_Pipelines/Delhi_High_Court.py
@@ -7,7 +7,7 @@ Functions Used
 2. judgementDate():Used for navigating to the judgement date page where date is to be entered.
 
 3. Calendar(D,M,Y,D2,M2,Y2):It takes date as input in DD MM YYYY format in which first from date is to be entered and then end date is to be entered for which it generates strings in format "DD/MM/YYYY" to be entered in 
-   date section in judgement date page.
+   date section in judgement date page. ## changes to be made as Calendar is updated from old one
 
 Working of Code :
 
@@ -18,7 +18,7 @@ After all the data is stored on server from that page then a try loop is called 
         
 
 """
-
+from datetime import date,timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -57,7 +57,10 @@ def page_reader():################# for Reading Contents of page when date is en
         case_title = col[3].text.replace("\n"," ").replace("\r"," ")
         case_petitioner = case_title.split("Vs")[0]
         case_respondent = case_title.split("Vs")[1]
-        
+        case.case_id = case_number
+        dates = datefinder.find_dates(u)
+        for i in dates:
+            date = i
 
         for c in col:
         #     # print(c.text)
@@ -70,10 +73,7 @@ def page_reader():################# for Reading Contents of page when date is en
                 case_url = a_tag.get_attribute("href")
                 
                 judgement_text = extract_txt(case_url, "Delhi_High_Court_Extract.pdf")
-        case.case_id = case_number
-        dates = datefinder.find_dates(u)
-        for i in dates:
-            date = i
+        
         case.date = date
         case.source = "Delhi High Court"
         case.url = case_url
@@ -82,80 +82,45 @@ def page_reader():################# for Reading Contents of page when date is en
         case.title = case_title
         case.respondent = case_respondent
         case.process_text()
-        # case.print_case_attributes()
-        store_case_document(case)
+        case.print_case_attributes()
+        # store_case_document(case)
         
         
 
 
-        
+###############  navigate to JudgementDate Page ####################################        
 
              
              
 
-def judgementDate():###############  navigate to JudgementDate Page ####################################
+def judgementDate():
     driver.get("http://164.100.69.66/jsearch/")
     # print(driver.title)
     link = driver.find_element_by_xpath("/html/body/table[2]/tbody/tr/td[@class='style13'][3]/b/input[@class='btn']")
     link.click()
 
 
-def Calendar(D,M,Y,D2, M2, Y2):######  for date iteration to be entered ##############################
 
-    D = int(D)
-    M = int(M)
-    Y = int(Y)
-    D2 = int(D2)
-    M2 = int(M2)
-    Y2 = int(Y2)
-
-    startm=M
-    k=0
-    a=""
-    date=[]
-    while M!=M2 and Y!=Y2 and M<13:
-        k=32-D
-        q=13-M
-        for j in range(q):
-            for i in range(k):
-                a = str(D+i)+"/"+str(M)+"/"+str(Y)
-                date.append(a)
-
-            M=M+1
-            M=M%13
-        Y = Y+1
-    if M==0:
-        M=M+1
-    while M!=M2 and Y==Y2:
-        k= 32-D
-        if (M==startm):
-            for i in range(k):
-                a = str(D+i)+"/"+str(M)+"/"+str(Y)
-                date.append(a)
-            M=M+1
-        else:
-            for i in range(k):
-                a = str(1+i)+"/"+str(M)+"/"+str(Y)
-                date.append(a)
-            M=M+1
-
-    if M==0:
-        M=M+1
-    if M==M2 and Y==Y2:
-        k = D2-D
-        for i in range(k):
-            a = str(D+i)+"/"+str(M)+"/"+str(Y)
-            date.append(a)
-    # print(w)
-    return date
+###############################################################
             
+def Calendar():
+    dates = []
+    today = date.today()
+    todays_date = today.strftime("%d/%m/%Y")
+    week_past = today - timedelta(days=7)
+    week_past_date = week_past.strftime("%d/%m/%Y")
+    
+    diff = today - week_past       
 
+    for i in range(diff.days + 1):
+        day = today - timedelta(days=i)
+        date_str = day.strftime("%d/%m/%Y")
+        dates.append(date_str)
+    return dates
 
-D,M,Y = input().split()          
-D2,M2,Y2 = input().split()
-w=Calendar(D,M,Y,D2,M2,Y2)
+############################################################################################
 
-
+w=Calendar()
 
 
 for i in range(len(w)):
@@ -172,7 +137,7 @@ for i in range(len(w)):
         submit_btn.click()
         driver.switch_to.default_content()
         
-        frame = driver.find_element_by_name("dynfr")############################for grasping data##########
+        frame = driver.find_element_by_name("dynfr")
         driver.switch_to.frame(frame)
         
         page_reader()
