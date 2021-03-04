34ad1edb56cc6389360c0f8709c092be5ec453fd
Utkarsh Chouksay <utkarshc611@gmail.com>
2021-03-04 22:59:34 +0530

    Update
    -Bombay HC created
    -Singapore SC created

---
+++ Court_Data_Pipelines/Bombay_HC.py
@@ -0,0 +1,202 @@
+from selenium import webdriver
+import time
+import datetime
+from Common_Files.Case_pdf_handling import extract_txt
+from Common_Files.Case_storage import store_case_document
+from Common_Files.Case_handler import CaseDoc 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located
+
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+#setting up the driver
+PATH='C://Program Files (x86)//chromedriver.exe'
+#driver= webdriver.Chrome(PATH)
+
+#Headless
+driver = webdriver.Chrome(PATH,chrome_options=options) 
+
+#opening an instance @bombay HC
+driver.get('https://bombayhighcourt.nic.in/')
+
+
+#accessing services -> orders & judgement -> rept. judgement/orders
+#services
+driver.find_element_by_xpath('//*[@id="smoothmenu1"]/ul[4]/li/a').click()
+time.sleep(1)
+
+#orders_and_judgement
+driver.find_element_by_xpath('/html/body/div[2]/table/tbody/tr[2]/th/table/tbody/tr/td/div/ul[4]/li/ul/li[2]/a').click()
+time.sleep(1)
+
+#Rept_judgement/orders
+driver.find_element_by_xpath('//*[@id="smoothmenu1"]/ul[4]/li/ul/li[2]/ul/li[3]/a').click()
+time.sleep(1)
+
+
+#accessing respective fields 
+
+#field-1--Side
+#then selecting desired side
+side=driver.find_element_by_xpath('/html/body/form/table[2]/tbody/tr/td[2]/select').click()
+time.sleep(1)
+
+side_select=driver.find_element_by_xpath('/html/body/form/table[2]/tbody/tr/td[2]/select/option[1]').click()
+
+
+
+#field-2--Act
+#then selecting desired act
+act=driver.find_element_by_xpath('/html/body/form/table[3]/tbody/tr/td[3]/select').click()
+time.sleep(1)
+
+act_select=driver.find_element_by_xpath('/html/body/form/table[3]/tbody/tr/td[3]/select/option[1]').click()
+
+
+
+#field-3--date
+#then filling desired dates
+current_day=datetime.date.today()
+week_control=current_day-datetime.timedelta(days=7)
+from_date=week_control.strftime('%d-%m-%Y')
+
+#curretnt date
+to_date=datetime.date.today().strftime('%d-%m-%Y')
+
+#entering from date into the box
+search_from_date=driver.find_element_by_xpath('//*[@id="demo1"]')
+driver.execute_script('document.getElementsByName("frmdate")[0].removeAttribute("onfocus")')
+search_from_date.send_keys(from_date)
+time.sleep(1)
+
+#entering to date into the box
+search_to_date=driver.find_element_by_xpath('//*[@id="demo2"]')
+driver.execute_script('document.getElementsByName("todate")[0].removeAttribute("onfocus")')
+search_to_date.send_keys(to_date)
+time.sleep(1)
+
+
+
+#field-4--Security code--Captcha
+security_code=driver.find_element_by_xpath('/html/body/form/table[6]/tbody/tr/td[2]/img').get_attribute("alt")
+enter_text=driver.find_element_by_xpath('/html/body/form/table[6]/tbody/tr/td[2]/input[2]')
+enter_text.send_keys(security_code)
+time.sleep(1)
+
+#submit button
+driver.find_element_by_xpath('/html/body/form/table[7]/tbody/tr/td[1]/input').click()
+time.sleep(2)
+
+
+
+#scrapping page's data
+#loacting target table for scrapping
+
+table=driver.find_element_by_xpath('/html/body/form/table[2]')
+t_body=table.find_element_by_tag_name('tbody')
+
+rows=t_body.find_elements(By.TAG_NAME,'tr')
+
+#column_headings=['Coram','Party','Judgment date and Bench','Case no. and Upload date']
+
+row_counter=0
+total_rows=0
+
+#accounting total no. of rows
+for row in rows:
+    total_rows+=1
+
+#traversing through rows of the table
+for row in rows:
+    
+    row_counter+=1
+
+    #skipping extra (starting, in-between and ending) rows 
+    if row_counter>=5 and row_counter%2!=0 and row_counter<total_rows-1: 
+        
+        td_counter=0
+        total_td=0
+        
+        tds=row.find_elements(By.TAG_NAME,'td')
+
+        #caculating no. of td(s)
+        for z in tds:
+            total_td+=1
+
+        #traversing through data points
+        for c in tds:
+            td_counter+=1
+
+
+            #printing field specific data
+            #coram field                
+            if td_counter==2:
+                temp=[]
+                temp.append(c.text)
+                temp_str=temp[0]
+                print('Judge(s)        :',temp_str)
+
+
+            #parties distinguished
+            if td_counter==3:
+                temp=[]
+                temp.append(c.text)
+                temp_str=temp[0]
+                indice_1=temp_str.find('Vs')
+                print('Petitioner Name :',temp_str[:indice_1])
+                print('Respondent Name :', temp_str[indice_1+3:])
+
+
+            #judgement date and bench seperation
+            if td_counter==4:
+                temp=[]
+                temp.append(c.text)
+                temp_str=temp[0]
+                
+                print('Judgement Date  :',temp_str[:10])
+                print('Bench           :',temp_str[11:])
+
+
+            #case no. and pdf seperation
+            if td_counter==5:
+                temp=[]
+                temp.append(c.text)
+                temp_str=temp[0]
+                indice_1=temp_str.find('(')
+                indice_2=temp_str.find(')')
+                
+                print('Case Number     :', temp_str[:indice_1])
+                #print('Upload Date    :', temp_str[indice_2+2:])
+
+                a_tags = c.find_elements(By.TAG_NAME,"a")
+                for a_tag in a_tags:
+                    print('Pdf link        :',a_tag.get_attribute('href'))                        
+
+        #spacing between cases        
+        print()
+
+#closing window/instance
+driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Court_Data_Pipelines/Singapur_Court_2.py
@@ -0,0 +1,100 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located
+
+
+#setting up the driver
+
+PATH='C://Program Files (x86)//chromedriver.exe'
+driver= webdriver.Chrome(PATH)
+
+#opening an instance @Singapore Court
+driver.get('https://www.supremecourt.gov.sg/news/supreme-court-judgments')
+
+#page counter
+#pg_counter=0
+
+#scrapping page's data
+while True:
+    
+    #pg_counter+=1
+    
+    #accessing target(ul) 
+    target_ul=driver.find_element_by_xpath('/html/body/form/div[4]/section[2]/div/div[2]/div[3]/ul[1]')
+    lis=target_ul.find_elements(By.TAG_NAME,'li')
+
+    #counter for elements to be scrapped from a single page
+    txt_counter=1
+
+    for li in lis:
+        
+        #accessing internal div(s)
+        if txt_counter<9:
+            
+            #accessing target div tag
+            txt=li.find_element_by_xpath(str('//*[@id="ContentPlaceHolderContent_C008_DivCode"]/ul[1]/li['+str(txt_counter)+']/div/div[1]'))
+            
+            #accessing target a tag
+            a_tag=li.find_element_by_xpath(str('/html/body/form/div[4]/section[2]/div/div[2]/div[3]/ul[1]/li['+str(txt_counter)+']/div/div[2]/a'))
+            pdf_link=a_tag.get_attribute('href')
+
+            temp_list=[]
+            temp_list.append(txt.text)
+            temp_str=temp_list[0]
+            indice_1=temp_str.find('\n')
+            
+            all_data_str=temp_str[indice_1+1:]
+            
+            #slicing string according to the entry fields
+            
+            #Petioner-name and respondent-name
+            indice_2=all_data_str.find('v')
+            indice_0=all_data_str.find('[')    
+            print('Petitioner Name :', all_data_str[:indice_2])
+            print('Respondent Name :', all_data_str[indice_2+2:indice_0-1])
+
+            #excluding pet. and resp. names
+            slice_1=all_data_str[all_data_str.find('\n')+1:]
+
+            #filing date and bench
+            indice_3=slice_1.find('[')
+            indice_4=slice_1.find(']')
+            indice_5=slice_1.find('DECISION')
+            
+            print('Filing Date     :', slice_1[indice_3+1:indice_4])
+            print('Bench           :', slice_1[indice_4+2:indice_5])
+            
+            #excluding f-date & bench
+            slice_2=slice_1[indice_5:]
+
+            #decision date
+            indice_6=slice_2.find(':')
+            print('Decision Date   :', slice_2[indice_6+2:indice_6+13])
+
+            print('Case number     :',slice_2[indice_6+14:])
+
+            #pdf link
+            print('Pdf link        :',pdf_link)
+
+            txt_counter+=1
+            print()
+    '''        
+    else:
+        print('..............................................................PAGE_NO_',pg_counter,'COMPLETE........................................')
+    '''
+        
+        #clicking next pg button
+        driver.find_element_by_xpath('/html/body/form/div[4]/section[2]/div/div[2]/div[3]/div[4]/div[3]/a').click()
+        time.sleep(2)
+        
+        
+
+
+
+
+
+
