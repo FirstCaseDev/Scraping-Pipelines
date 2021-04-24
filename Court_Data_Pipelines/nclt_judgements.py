dbc08ab140f423355d07c1f2c67699ac863fb7ba
Utkarsh Chouksay <utkarshc611@gmail.com>
2021-04-24 15:39:53 +0530

    Update
    nclt_judgements-->added code for accessing

--- Court_Data_Pipelines/nclt_judgements.py
+++ Court_Data_Pipelines/nclt_judgements.py
@@ -13,11 +13,98 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.expected_conditions import presence_of_all_elements_located
 
-
 #accessing Case_storage database
 client = pymongo.MongoClient('mongodb://db_user:firstCaseDevTeam@107.20.44.181:27017,3.229.151.98:27017,54.175.129.116:27017/?authSource=admin&replicaSet=aName&readPreference=primaryPreferred&ssl=false')
 db = client["indian_court_data"]
 col = db["cases"]
+
+
+def nclt_jud_scrapper(bench_name):
+    #scrapping page's data
+    case=CaseDoc()
+    
+    #loacting target table for scrapping
+    table=driver.find_element_by_xpath('//*[@id="block-system-main"]/div/div/div[1]/table')
+    t_body=table.find_element_by_tag_name('tbody')
+    rows=t_body.find_elements(By.TAG_NAME,'tr')
+    
+    row_counter=0
+    for row in rows:
+        row_counter+=1
+
+        if row_counter>0:
+
+            td_counter=0
+            
+            tds=row.find_elements(By.TAG_NAME,'td')
+            
+
+            #bench name and source
+            #print('Bench                   :', bench_name)
+            case.bench = bench_name
+            case.source = 'National Company Law Tribunal'
+
+
+            for td in tds:
+                td_counter+=1
+
+                #case number --> Diary No. 
+                if td_counter==2:
+                    temp=[]
+                    temp.append(td.text)
+                    #print('Case id                 :', temp[0])
+                    case.case_id = temp[0]
+
+
+                #petitioner & respondent
+                if td_counter==3:
+                    temp=[]
+                    temp.append(td.text)
+                    temp_str=temp[0]
+                    slice_1=temp_str.find('Vs')
+                    #slice_2=temp_str.find('\n')
+                    slice_3=temp_str.find('Respondent Advocate')
+                    #print(temp)
+                    #print('Petitioner              :', temp_str[0:slice_1])
+                    #print('Respondent              :', temp_str[slice_1+3:slice_3-1])
+                    #print('Respondent Advocate     :', temp_str[slice_2+1:slice_3-1])
+                    case.petitioner = temp_str[0:slice_1]
+                    case.respondent = temp_str[slice_1+3:slice_3-1]
+
+
+                #date of judgement
+                if td_counter==4:
+                    temp=[]
+                    temp.append(td.text)
+                    #print('Date of Judgement       :', temp[0])
+                                            
+                    date=datefinder.find_dates(temp[0])
+                    for i in date:
+                        date=i 
+                    case.date=date
+                    case.year = date.strftime("%Y")
+                    
+                
+                #pdf link
+                if td_counter==5:
+                    a_tags = td.find_elements(By.TAG_NAME,"a")
+                    for a_tag in a_tags:
+                        #print('Pdf link                :', a_tag.get_attribute('href'))
+                        pdf_link=a_tag.get_attribute('href')
+                        case.url=pdf_link
+                        judgement_txt=extract_txt(pdf_link, 'Court_Extract.pdf')
+                        case.judgement_text=judgement_txt
+                        
+            #spacing b/w judgemnets
+            print()            
+
+            #processing all the extracted data
+            case.process_text()
+            case.print_case_attributes()
+            #store_case_document(case)
+                
+      
+
 
 
 #for headless
@@ -34,17 +121,18 @@ driver= webdriver.Chrome(PATH)
 #driver = webdriver.Chrome(PATH,chrome_options=options) 
 
 #opening an instance @NCLT.GOV.IN
-driver.get('https://nclt.gov.in/')
+driver.get('https://nclt.gov.in/exposed-judgement-date-wise-page')
 time.sleep(1)
 
+'''
 #judgements --> judgement date wise
 driver.find_element_by_xpath('//*[@id="navbarResponsive"]/ul/li[4]/a').click()
 driver.find_element_by_xpath('//*[@id="node-297045"]/div/div/div/div/ul/li[1]/a').click()
 time.sleep(1)
-
+'''
 #setting start/end date- (past 1 week)
 current_day=datetime.date.today()
-week_control=current_day-datetime.timedelta(days=7)
+week_control=current_day-datetime.timedelta(days=90)
 start_date=week_control.strftime('%d/%m/%Y')
 temp=start_date[:-4]+start_date[-2:]
 #print(temp)
@@ -66,7 +154,7 @@ end_day_box.send_keys(end_date)
 
 
 #loop for diffrent benches(21 in total)
-for benches in range(2,23):
+for benches in range(2,4):
 
     #accessing final judgements --> Bench wise
     driver.find_element_by_xpath('//*[@id="edit-field-bench-target-id"]').click()
@@ -80,6 +168,7 @@ for benches in range(2,23):
 
     #scrapping page's data
     case=CaseDoc()
+
     #loacting target table for scrapping
     try:
         table=driver.find_element_by_xpath('//*[@id="block-system-main"]/div/div/div[1]/table')
@@ -111,7 +200,7 @@ for benches in range(2,23):
                 '''
 
                 #bench name and source
-                print('Bench                   :', bench_name)
+                #('Bench                   :', bench_name)
                 case.bench = bench_name
                 case.source = 'National Company Law Tribunal'
 
@@ -132,15 +221,15 @@ for benches in range(2,23):
                         temp=[]
                         temp.append(td.text)
                         temp_str=temp[0]
-                        slice_1=temp_str.find(' Vs ')
-                        slice_2=temp_str.find('\n')
+                        slice_1=temp_str.find('Vs')
+                        #slice_2=temp_str.find('\n')
                         slice_3=temp_str.find('Respondent Advocate')
                         #print(temp)
                         #print('Petitioner              :', temp_str[0:slice_1])
-                        #print('Respondent              :', temp_str[slice_1+4:slice_2])
+                        #print('Respondent              :', temp_str[slice_1+3:slice_3-1])
                         #print('Respondent Advocate     :', temp_str[slice_2+1:slice_3-1])
                         case.petitioner = temp_str[0:slice_1]
-                        case.respondent = temp_str[slice_1+4:slice_2]
+                        case.respondent = temp_str[slice_1+3:slice_3-1]
 
 
                     #date of judgement
@@ -173,13 +262,56 @@ for benches in range(2,23):
                 case.process_text()
                 case.print_case_attributes()
                 #store_case_document(case)
+
+
+        #code for accessing next pages
+        # will access 2nd last li tag (next) to go to the next page
+
+        #calculating total no. of li tags(lis, li_counter) 
+        ul=driver.find_element_by_xpath('//*[@id="block-system-main"]/div/div/div[2]/ul')        
+        lis=ul.find_elements(By.TAG_NAME, 'li')
+        li_counter=0
+        for li in lis:
+            li_counter+=1
+
+        second_last_li_index = li_counter-1  
+
+        k= True
+        nxt_pg_counter=0
+
+        while k:               
+            if (driver.find_element_by_xpath('//*[@id="block-system-main"]/div/div/div[2]/ul/li[{next_index}]/a'.format(next_index=second_last_li_index)) and (driver.find_element_by_xpath('//*[@id="block-system-main"]/div/div/div[2]/ul/li[{next_index}]/a'.format(next_index=second_last_li_index)).text == 'next ›')): 
+                nxt_pg_counter+=1
+                driver.find_element_by_xpath('//*[@id="block-system-main"]/div/div/div[2]/ul/li[{next_index}]/a'.format(next_index=second_last_li_index)).click()
+                
+                #for countering first/prev li tags
+                #calculating second_last_li_index for new pg
+                ul=driver.find_element_by_xpath('//*[@id="block-system-main"]/div/div/div[2]/ul')        
+                lis=ul.find_elements(By.TAG_NAME, 'li')
+                li_counter=0
+                for li in lis:
+                    li_counter+=1
+
+                second_last_li_index = li_counter-1
+                
+                #calling scrapper
+                nclt_jud_scrapper(bench_name)
+                time.sleep(2)
+
+            else:
+                k=False
+                #print('going back to pg-1')
+                driver.execute_script('window.history.go({0})'.format(-(nxt_pg_counter)))
+                time.sleep(1)
                 
         #back to bench selection page
+        #print('going back to bench selection pg')
         driver.back()
 
     except:
+        #back to bench selection page
+        #print('going back to bench selection pg--')
         driver.back()
 
 #closing chrome istance/window
 driver.quit()
-
